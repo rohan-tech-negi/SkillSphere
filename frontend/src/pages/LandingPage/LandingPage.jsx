4c952737f6cbae7d9cff4e6b27d74c5b6cbd8643
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-22 14:23:30 +0530

    few details be adding in the lanfing page .jsx

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
 import {motion} from "framer-motion"
 import {Briefcase} from "lucide-react"
-import {useNavigation} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 const LandingPage = () => {
+  const isAuthenticated = true;
+  const user = {fullName:"alex", role:"employer"}
+  const navigate = useNavigate()
   return (
-    <div>
+    <header>
+      <div className=''>
+        <div className="">
+          {/* logo */}
+          <div className=''>
+            <div className=''>
+              <Briefcase className=''>
+              </Briefcase>
+            </div>
+            <span className=''>
+              JobPortal
+            </span>
+          </div>
 
-    </div>
+          {/* navigation links */}
+          <nav className='flex items-center justify-end gap-x-6'>
+            <a href="" onClick={()=> {
+              navigate(
+                isAuthenticated && user?.role === "employer" ? "/employer-dashboard" : "/login"
+              )
+            }} className=''> For Employers
+
+            </a>
+            <nav>
+
+            </nav>
+          </nav>
+
+          {/* auth buttons */}
+          <div className=''>
+            {isAuthenticated ? (
+              <div className=''></div>
+              <span className=''> Welcome, {user?.fullName}</span>
+              <a href={user?.role === "employer"} ></a>
+            )}
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
