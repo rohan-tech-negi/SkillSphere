69ff3e463c639aa6074815684c1526e4c0ba3a2f
rohan-tech-negi <rohanwork953@gmail.com>
2026-04-22 14:14:34 +0530

    now be working on thje landing page

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import {motion} from "framer-motion"
+import {Briefcase} from "lucide-react"
+import {useNavigation} from "react-router-dom"
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div>
+
+    </div>
   )
 }
 
